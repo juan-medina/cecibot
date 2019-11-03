0cec7fd69ba128ea36f3906a11dabde71f3b117f
Juan Medina <jamedina@gmail.com>
2019-11-03 08:49:19 +0000

    adding help to commands

--- processor/processor.go
+++ processor/processor.go
@@ -1,7 +1,10 @@
 package processor
 
 import (
+	"fmt"
 	"github.com/juan-medina/cecibot/prototype"
+	"strings"
+	"unicode"
 )
 
 type Processor interface {
@@ -9,15 +12,67 @@ type Processor interface {
 	Init(bot prototype.Bot) error
 	End()
 }
+type commandFunction func(args []string, author string) string
+type command struct {
+	key  string
+	desc string
+	fun  commandFunction
+	help string
+}
 
 type defaultProcessor struct {
-	bot   prototype.Bot
-	owner string
+	bot      prototype.Bot
+	owner    string
+	commands map[string]command
+	help     string
+}
+
+func (p *defaultProcessor) AddCommand(key string, fun commandFunction, desc string, help string) {
+	p.commands[key] = command{
+		key:  key,
+		desc: desc,
+		fun:  fun,
+		help: help,
+	}
 }
 
 func (p *defaultProcessor) Init(bot prototype.Bot) error {
 	p.bot = bot
 	p.owner = bot.GetConfig().GetOwner()
+
+	p.AddCommand("ping", func(args []string, author string) string {
+		return "pong!"
+	}, "Asks for a ping to the *bot*.", "This is a test command for the *bot* that will reply with a pong message")
+
+	p.AddCommand("hello", func(args []string, author string) string {
+		if p.isOwner(author) {
+			return "hello master!"
+		}
+		return "hello!"
+	}, "Greets the *user*.", "This command will greet *you* back.")
+
+	p.AddCommand("help", func(args []string, author string) string {
+		argc := len(args)
+		if argc > 0 {
+			key := args[0]
+			cmd, found := p.commands[key]
+			if found {
+				return fmt.Sprintf("Command **%s** : \n%s", key, cmd.help)
+			}
+
+			return "Unknown command in help. " + p.help
+		}
+		return p.help
+
+	}, "Gets help with *commands*.", "Usage:\n\t**help** *command*\n\nUse this command to get help with any *command*.")
+
+	help := "Available commands are:"
+	for key, cmd := range p.commands {
+		help += fmt.Sprintf("\n\t **%s** : %q", key, cmd.desc)
+	}
+	help += "\n\nTo get help on any *command* send:\n\t**help** *command*"
+	p.help = help
+
 	return nil
 }
 
@@ -25,24 +80,67 @@ func (p defaultProcessor) End() {
 }
 
 func New() *Processor {
-	var prc Processor = &defaultProcessor{}
+	var prc Processor = &defaultProcessor{commands: make(map[string]command)}
 	return &prc
 }
 
 func (p defaultProcessor) isOwner(author string) bool {
 	return p.owner == author
 }
-func (p defaultProcessor) ProcessMessage(text string, author string) string {
-	if text == "ping" {
-		return "pong!"
-	} else if text == "pong" {
-		return "ping!"
-	} else if text == "hello" {
-		if p.isOwner(author) {
-			return "hello master!"
+
+func (p defaultProcessor) parseCommand(text string) (key string, args []string) {
+	var m []string = nil
+	var s string
+
+	str := strings.TrimSpace(text) + " "
+
+	var lastQuote int32 = 0
+	isSpace := false
+	for i, c := range str {
+		switch {
+		// If we're ending a quote, break out and skip this character
+		case c == lastQuote:
+			lastQuote = 0
+
+		// If we're in a quote, count this character
+		case lastQuote != 0:
+			s += string(c)
+
+		// If we encounter a quote, enter it and skip this character
+		case unicode.In(c, unicode.Quotation_Mark):
+			isSpace = false
+			lastQuote = c
+
+		// If it's a space, store the string
+		case unicode.IsSpace(c):
+			if 0 == i || isSpace {
+				continue
+			}
+			isSpace = true
+			m = append(m, s)
+			s = ""
+
+		default:
+			isSpace = false
+			s += string(c)
 		}
-		return "hello!"
+
 	}
 
-	return ""
+	if m == nil || lastQuote != 0 {
+		return "", []string{}
+	}
+
+	return m[0], m[1:]
+}
+
+func (p defaultProcessor) ProcessMessage(text string, author string) string {
+
+	key, args := p.parseCommand(text)
+	cmd, found := p.commands[key]
+	if found {
+		return cmd.fun(args, author)
+	}
+
+	return "Unknown command. " + p.help
 }
--- processor/processor_test.go
+++ processor/processor_test.go
@@ -2,6 +2,7 @@ package processor
 
 import (
 	"github.com/juan-medina/cecibot/config"
+	"reflect"
 	"testing"
 )
 
@@ -32,6 +33,11 @@ func TestDefaultProcessor_ProcessMessage(t *testing.T) {
 	cfg := fakeCfg{}
 	bot := fakeBot{cfg: cfg}
 
+	proc := *New()
+	_ = proc.Init(bot)
+
+	help := proc.(*defaultProcessor).help
+
 	type testCase struct {
 		name string
 		text string
@@ -43,12 +49,6 @@ func TestDefaultProcessor_ProcessMessage(t *testing.T) {
 			"ping command",
 			"ping",
 			"pong!",
-			"6789",
-		},
-		{
-			"pong command",
-			"pong",
-			"ping!",
 			"6789",
 		},
 		{
@@ -64,25 +64,118 @@ func TestDefaultProcessor_ProcessMessage(t *testing.T) {
 			cfg.GetOwner(),
 		},
 		{
+			"help command",
+			"help",
+			help,
+			"6789",
+		},
+		{
+			"help a concrete command",
+			"help ping",
+			"Command **ping** : \nThis is a test command for the *bot* that will reply with a pong message",
+			"6789",
+		},
+		{
+			"help a unknown command",
+			"help zzz",
+			"Unknown command in help. " + help,
+			"6789",
+		},
+		{
 			"invalid command",
 			"zzz",
-			"",
+			"Unknown command. " + help,
 			"6789",
 		},
 	}
 
-	proc := defaultProcessor{}
-	_ = proc.Init(bot)
-
 	for _, tt := range cases {
-		got := proc.ProcessMessage(tt.text, tt.user)
-
 		t.Run(tt.name, func(t *testing.T) {
+			got := proc.ProcessMessage(tt.text, tt.user)
 			if got != tt.want {
-				t.Errorf("processor error got %q, want %q", got, tt.want)
+				t.Errorf("processor error want %q, got %q", tt.want, got)
 			}
 		})
 
 	}
 	proc.End()
+}
+
+func TestDefaultProcessor_parseCommand(t *testing.T) {
+	proc := defaultProcessor{}
+
+	type testCase struct {
+		name string
+		text string
+		cmd  string
+		args []string
+	}
+
+	cases := []testCase{
+		{
+			name: "empty command",
+			text: "",
+			cmd:  "",
+			args: []string{},
+		},
+		{
+			name: "one word command",
+			text: "hello",
+			cmd:  "hello",
+			args: []string{},
+		},
+		{
+			name: "two words commands",
+			text: "hello world",
+			cmd:  "hello",
+			args: []string{"world"},
+		},
+		{
+			name: "thee words commands",
+			text: "hello world command",
+			cmd:  "hello",
+			args: []string{"world", "command"},
+		},
+		{
+			name: "command with single quotes",
+			text: "hello 'big world'",
+			cmd:  "hello",
+			args: []string{"big world"},
+		},
+		{
+			name: "command with double quotes",
+			text: "hello \"big world\"",
+			cmd:  "hello",
+			args: []string{"big world"},
+		},
+		{
+			name: "command with mixed quotes",
+			text: "hello \"big world\" 'with some more quotes'",
+			cmd:  "hello",
+			args: []string{"big world", "with some more quotes"},
+		},
+		{
+			name: "command with broken quotes",
+			text: "hello 'big world",
+			cmd:  "",
+			args: []string{},
+		},
+	}
+
+	for _, tt := range cases {
+		t.Run(tt.name, func(t *testing.T) {
+			gotCmd, gotArgs := proc.parseCommand(tt.text)
+
+			if gotCmd != tt.cmd {
+				t.Errorf("want %q, got %q", tt.cmd, gotCmd)
+				return
+			}
+
+			if reflect.DeepEqual(gotArgs, tt.args) == false {
+				t.Errorf("want %v, got %v", tt.args, gotArgs)
+				return
+			}
+		})
+	}
+
 }
